7170e99166b23ed389904385cce1c34a933b3e70
benaroya <laurent.benaroya@gmail.com>
2023-02-18 21:44:30 +0100

    from text; tuned parameters

--- wordcloudmaker.py
+++ wordcloudmaker.py
@@ -1,145 +1,55 @@
-import io
-import pandas as pd
-import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-
-import nltk
-from nltk.corpus import stopwords
-from textblob import Word
 from wordcloud import WordCloud
-
-
-def processRow(row):
-    tweet = row
-    tweet = tweet.lower()
-
-    tweet = re.sub(r'(\\u[0-9a-fA-F]+)', r'', tweet)
-    # tweet = re.sub(r'[^\x00-\x7f]', r'', tweet)
-
-    tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)  # ^\s
-    tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
-
-    tweet = re.sub('[\s]+', ' ', tweet)
-    tweet = re.sub('[\n]+', ' ', tweet)
-    tweet = re.sub('[\t]+', ' ', tweet)
-
-    tweet = re.sub(r'[^\w]', ' ', tweet)
-
-    tweet = re.sub(r'(#[^\s]+)', r'\1', tweet)
-    tweet = ' '.join([i for i in tweet.split() if not i.isdigit()])
-
-    tweet = re.sub(r'(\.)\1+', ' ', tweet)
-    # lemma
-    tweet = ' '.join([Word(word).lemmatize() for word in tweet.split()])
-
-    # trim
-    tweet = tweet.strip('\'''')
-    row = tweet
-
-    return row
 
 
 if __name__ == "__main__":
 
     import argparse
 
-    parser = argparse.ArgumentParser(description="generate wordcloud from CV in pdf format")
-    parser.add_argument('--cv', type=str,
-                        required=True, help='input CV in pdf format')
+    parser = argparse.ArgumentParser(description="generate wordcloud from ordered words/group of words text file")
+    parser.add_argument('--txt', type=str,
+                        required=True, help='input text file')
     parser.add_argument('--img', type=str,
                         required=True, help='filename of the output wordcloud image')
-    parser.add_argument('--lang', type=str,
-                        required=True, help='filename of the output wordcloud image (english or french)')                        
-    parser.add_argument('--com', type=int, default=10,
-                        required=False, help='number of most common words (they are not taken into account)')                        
-    parser.add_argument('--map', type=str, default=None,
+    parser.add_argument('--lang', type=str, default='french',
+                        required=False, help='filename of the output wordcloud image (english or french)')                                               
+    parser.add_argument('--map', type=str, default='Blues',
                         required=False, help='name of the figure colormap')
-    parser.add_argument('--hist', action='store_true', default = False,
-                        required=False, help='compute the histogram of the number of occurence of words')
-
+    """
+    'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'
+    """
     args = parser.parse_args()
 
-    # read pdf
-    resMgr = PDFResourceManager()
-    retData = io.StringIO()
-    TxtConverter = TextConverter(resMgr, retData, laparams= LAParams())
-    interpreter = PDFPageInterpreter(resMgr, TxtConverter)
+    lines = open(args.txt).readlines()
+    word_dict = dict()
+    for count, line in enumerate(lines):
+        line = line.strip()
+        word_dict[line] = count+1
 
-    with open(args.cv,'rb') as fid:
-        for page in PDFPage.get_pages(fid):
-            interpreter.process_page(page)
-    txt = retData.getvalue()
-    docu = txt.split(' ')
-    # set as dataframe
-    df = pd.DataFrame({'line': docu})
-    nltk.download('omw-1.4')
-    
-    # get words and remove words with less than 4 characters
-    cv_words = []
-    for line in df['line']:
-        for word in processRow(line).split():
-            if len(word) > 3:
-                cv_words.append(word)
-
-    print(f'number of words in CV : {len(cv_words)}')
-    # remove stop words
-    stopWords = stopwords.words(args.lang)
-    cv_words2 = [word for word in cv_words if word not in stopWords]
-    if True:
-        # ad-hoc for Laurent E.
-        cv_words2 = [word.replace('commerciale', 'commercial') for word in cv_words2]
-    # compute word frequency
-    frequency_dist = nltk.FreqDist(cv_words2)
-
-    print('fréquence des xx premiers mots les plus utilisés')
-    print(frequency_dist.most_common(args.com))
+    T = len(word_dict)
+    alpha = 0.9
+    z_min = 3
+    z_max = 10
+    delta = z_min-z_max
+    modified_word_dict = dict()
+    frequency_dist = dict()
+    for word, value in word_dict.items():
+        if False:
+            print(word)
+            print(value)
+        frequency_dist[word] = int(np.floor(delta*((value-1)/(T-1))+z_max))
 
     # generate wordcloud
-    wcloud = WordCloud(colormap=args.map).generate_from_frequencies(frequency_dist)
-    # wcloud = WordCloud(colormap="coolwarm").generate_from_frequencies(frequency_dist)
-
-    # histogram
-    if args.hist:
-        # count word occurences
-        numwordshist = dict() 
-        for k, v in frequency_dist.items(): 
-            if v not in numwordshist.keys(): 
-                numwordshist[v] = 1 
-            else: 
-                numwordshist[v] += 1 
-        maxrep = max(numwordshist.keys())
-        # to numpy vector
-        numwordhist_np = np.zeros((maxrep+1,), dtype='int')
-        for k, v in numwordshist.items(): 
-            numwordhist_np[k] = v
-
-        # plot bar histogram
-        indexes = np.arange(1, maxrep+1)
-        values = numwordhist_np[1:]
-
-        fig, ax = plt.subplots(figsize=(15, 10)) 
-        
-        bars = ax.bar(indexes, values)      
-        ax.bar_label(bars) 
-        
-        ax.yaxis.set_ticklabels([])
-        
-        plt.title("histogramme de répétitions des mots")
-        plt.ylabel('nombre de mots répétés')
-        plt.xlabel('nombre de répétitions')
-        plt.savefig(args.img[:-4] + '_repetitions.png')
-        plt.show()
+    xfig = 6
+    yfig = 3
+    wcloud = WordCloud(colormap=args.map, prefer_horizontal=0.8, height=yfig*100, width=xfig*100).generate_from_frequencies(frequency_dist)
 
     # plot figure
-    plt.figure(figsize=(15, 10))
+    plt.figure(figsize=(xfig, yfig))
     plt.imshow(wcloud, interpolation='bilinear', origin='upper')
     plt.axis('off')
-    plt.margins(x=0, y=0)
+    plt.tight_layout(pad=0)
     plt.savefig(args.img)
     plt.show()
